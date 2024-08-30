4711138a3cfd15a03b5f130860922d1a4de23606
dracuusta <ssrinet10@gmail.com>
2024-08-30 18:57:22 +0530

    keymaps are better

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,11 +1,8 @@
-vim.g.mapleader = " "
-local o = vim.opt
-o.relativenumber = true
 local M = {}
-
 vim.g.mapleader = " "
 local o = vim.opt
 --editor options--
+o.relativenumber = true
 o.number = true
 o.autoindent = true
 o.cursorline = true --highlight screenline of cursor when starting a new line --
@@ -13,35 +10,31 @@ o.shiftwidth = 1
 o.tabstop = 4
 o.expandtab = true
 
-local nonoremap = function(mode, lhs, rhs, opts)
-  local options = {}
-  if opts then
-    options = opts
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
 
-nonoremap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-nonoremap("n", "<C-u", "<C-u>zz", { noremap = true, silent = true })
---remap auto keyword in vim--
+vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
+
+--NeoTree Toggles--
+vim.keymap.set("n", "<C-t>", ":Neotree toggle<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>e", ":Neotree toggle position=float<CR>", { noremap = true ,silent=true})
+vim.keymap.set("n", "<leader>h", ":Neotree toggle<CR>", {
+ noremap = true,silent=true,
+})
+
+--General--
+vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>", { noremap = true ,silent=true})
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {
   noremap = true,
   silent = true,
 })
-vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
-vim.keymap.set("n", "<C-t>", ":Neotree toggle<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>e", ":Neotree toggle position=float<CR>", { noremap = true ,silent=true})
---remap Neotree toggle--
-
-vim.keymap.set("n", "<leader>h", ":Neotree toggle<CR>", {
- noremap = true,silent=true,
-})
+nonoremap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+nonoremap("n", "<C-u", "<C-u>zz", { noremap = true, silent = true })
 -- Automatically save files when leaving insert mode or switching buffers
 vim.api.nvim_create_augroup("autosave", { clear = true })
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   group = "autosave",
   command = "silent! wall",
 })
+
 --buffer shortcuts--
 vim.keymap.set("n","<Tab>",":bnext<CR>",{remap=true,silent=true})
 vim.keymap.set("n","<S-Tab>",":bprev<CR>",{remap=true,silent=true})
---
+++ nvim/lua/plugins/nonLSP-lsp.lua
@@ -0,0 +1,3 @@
+return {
+    "digitaltoad/vim-pug"
+}
