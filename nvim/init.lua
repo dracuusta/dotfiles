8e206dcbab430df29497c1d0c05ade86a19cabcc
dracuusta <ssrinet10@gmail.com>
2025-01-14 23:19:33 +0530

    check

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 require("config.lazy")
 require("config.keymaps")
 require("mason").setup()
+
 require("netrw").setup({})
 require("lspconfig").ts_ls.setup({})
 require("lspconfig").pyright.setup({})
